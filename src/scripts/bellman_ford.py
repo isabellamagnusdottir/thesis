45b9eb3b7697afbfaddb11390843a4bb42ea50ae
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2025-04-02 13:31:49 +0200

    Add random negative cycless graph generator

--- src/scripts/bellman_ford.py
+++ src/scripts/bellman_ford.py
@@ -3,17 +3,35 @@ from numpy import inf
 from src.utils.cycle_error import NegativeCycleError
 
 
-def standard_bellman_ford(graph: dict[int, dict[int, int]], source: int):
+def standard_bellman_ford(graph: dict[int, dict[int, int]], source: int, with_parent = False):
     dist = [inf]*len(graph)
     dist[source] = 0
+    if with_parent: parent = [None] * len(graph)
     for _ in range(len(graph)-1):
         for u,neighborhood in graph.items():
             for v in neighborhood.keys():
                 if dist[u] + graph[u][v] < dist[v]:
                     dist[v] = dist[u]+graph[u][v]
+                    if with_parent: parent[v] = u
     
     for u,neighborhood in graph.items():
         for v in neighborhood.keys():
             if dist[u] + graph[u][v] < dist[v]:
-                raise NegativeCycleError
+                # TODO: Consider implementing finding the negative cycle using
+                # a predecessor array.
+                if with_parent:
+                    cycle = []
+                    visited = set()
+                    cur = v
+                    while cur not in visited:
+                        if cur is None:
+                            raise RuntimeError("Impossible state: Trying to fetch negative cycle that should exist")
+                        visited.add(cur)
+                        cycle.append(cur)
+                        cur = parent[cur]
+                    cycle_start = cycle.index(cur)
+                    cycle = cycle[cycle_start:] + [cur]
+
+                    raise NegativeCycleError(cycle)
+                raise NegativeCycleError()
     return dist
